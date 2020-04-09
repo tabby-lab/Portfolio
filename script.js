81118601497776c1769ec4821ef7079112e23670
Tabby Garcia <tabby.garcia1@gmail.com>
2020-04-09 18:13:37 -0500

    contact html and fixed alignment in index html

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-    $('.parallax').parallax();
-  });
+document.getElementById("submit-btn").addEventListener("click", function(){
+ var textArea= document.getElementById("textarea1").value
+ var email= document.getElementById("email").value
+ window.location.href="mailto:tabby.garcia1@gmail.com?subject=Info&body="+ textArea
+})
+
